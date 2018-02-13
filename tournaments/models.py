e76096b44ffb22bea8300be7aeb652c6886d6188
Tyler Trinh <tylervtrinh@gmail.com>
2018-02-13 13:06:02 -0800

    Add more models

--- tournaments/models.py
+++ tournaments/models.py
@@ -8,6 +8,20 @@ class Profile(models.Model):
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
+    wins = models.PositiveIntegerField()
+
+
+class Matches(models.Model):
+    winner = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    player1 = models.CharField(max_length=30)
+    player2 = models.CharField(max_length=30)
+    game-id = models.PositiveIntegerField()
+
+class Tourney(models.Model):
+
+    size = models.PositiveIntegerField()
+    name = models.CharField(max_length=30,blank=True)
+    winner = models.CharField(max_length=30,blank=True)
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -17,3 +31,8 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+
+
+
