12c2e99cad49dbd13d054acd1b66e5ff3f885edb
Tyler Trinh <tylervtrinh@gmail.com>
2018-02-13 15:36:54 -0800

    Fix final errors in models and allow admin to edit

--- tournaments/admin.py
+++ tournaments/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Profile, Tourney, Match
+
+admin.site.register(Profile)
+admin.site.register(Tourney)
+admin.site.register(Match)
--- tournaments/models.py
+++ tournaments/models.py
@@ -5,8 +5,9 @@ from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User,
-                                on_delete=models.CASCADE,)
-    wins = models.PositiveIntegerField()
+                                on_delete=models.CASCADE,
+                                null=True)
+    wins = models.PositiveIntegerField(default=0)
 
 class Tourney(models.Model):
     player = models.ManyToManyField(Profile)
@@ -16,14 +17,15 @@ class Tourney(models.Model):
     winner = models.ForeignKey(Profile,
                                on_delete=models.CASCADE,
                                null=True,
-                               blank=True,)
+                               blank=True,
+                               related_name='tourneywinner')
 
 class Match(models.Model):
     winner = models.ForeignKey(Profile,
                                on_delete=models.CASCADE,
                                null=True,
                                blank=True,
-                               related_name='winner',)
+                               related_name='matchwinner',)
     player1 = models.ForeignKey(Profile,
                                 on_delete=models.CASCADE,
                                 null=True,
@@ -39,17 +41,6 @@ class Match(models.Model):
                                 null=True,
                                 blank=False,
                                 related_name='tourney')
-
-class Tourney(models.Model):
-    player = models.ManyToManyField(Profile)
-    name = models.CharField(max_length=30,
-                            null=True,
-                            blank=False,)
-    winner = models.ForeignKey(Profile,
-                               on_delete=models.CASCADE,
-                               null=True,
-                               blank=True,
-                               related_name='winner',)
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
