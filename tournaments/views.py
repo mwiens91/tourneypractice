07763d641d21f73835ed37117d7a8aad1c751dda
Tyler Trinh <tylervtrinh@gmail.com>
2018-02-14 15:56:52 -0800

    Create default signup page

--- tournaments/views.py
+++ tournaments/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from .models import Tourney
 
@@ -14,3 +16,16 @@ def tourney(request,tourney_id):
     return render(request,'tourney.html', {'tourney': thistourney,
                                             'numplayers': numplayers})
 
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
